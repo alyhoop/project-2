815a05121da28b5c86db808e4f285664009b65f3
Alyssa Hooper <nesteralyssa@gmail.com>
2020-05-04 21:31:51 -0700

    css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
+class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      age: null,
+    };
+  }
+  myChangeHandler = (event) => {
+    let nam = event.target.name;
+    let val = event.target.value;
+    this.setState({[nam]: val});
+  }
   render() {
     return (
-    <div className="App">
-      <h1>placeholder</h1>
-    </div>
-     );
+      <form>
+      <h1>Hello {this.state.username} {this.state.age}</h1>
+      <p>Enter your name:</p>
+      <input
+        type='text'
+        name='username'
+        onChange={this.myChangeHandler}
+      />
+      <p>Enter your age:</p>
+      <input
+        type='text'
+        name='age'
+        onChange={this.myChangeHandler}
+      />
+      </form>
+    );
   }
 }
 
-export default App;
+export default MyForm;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+import MyForm from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <MyForm />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
